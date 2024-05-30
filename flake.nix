b2425caa2bd4f9b97b737b10def6863f85d58e02
Michael Wu <me@miXwui.com>
2024-05-30 06:16:31 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Framework 13 AMD 7840U NixOS config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.framenix = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [ 
+          ./configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+    };
+}
---
+++ main-user.nix
@@ -0,0 +1,36 @@
+{ lib, config, pkgs, ... }:
+
+let
+  cfg = config.main-user;
+in
+{
+  options.main-user = {
+    enable
+      = lib.mkEnableOption "enable user module";
+
+    username = lib.mkOption {
+      default = "mainuser";
+      description = ''
+        username
+      '';
+    };
+
+    fullname = lib.mkOption {
+    default = "First Last";
+    description = ''
+      name of user
+    '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    users.users.${cfg.username} = {
+      isNormalUser = true;
+      initialPassword = "12345";
+      description = cfg.fullname;
+      extraGroups = [ "networkmanager" "wheel" ];
+      packages = with pkgs; [];
+      shell = pkgs.fish;
+    };
+  };
+}
