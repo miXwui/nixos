e552ba823700e49e82535d5083854590c1b56ef2
Michael Wu <me@miXwui.com>
2024-05-30 06:21:47 -0500

    Add interruptible sl alias

--- configuration.nix
+++ configuration.nix
@@ -91,6 +91,11 @@
     tree
   ];
 
+  # https://stackoverflow.com/a/45027283
+  environment.interactiveShellInit = ''
+    alias sl = "sl -e"
+  '';
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
