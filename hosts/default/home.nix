5a24d31909b4af639c1e989b8e71e325f93b5376
Michael Wu <me@miXwui.com>
2024-06-02 03:15:28 -0500

    Import entire home-manager folder
    * Add/use default.nix

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -1,11 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ../../modules/home-manager/git.nix
-    ../../modules/home-manager/sway.nix
-    ../../modules/home-manager/fish.nix
-  ];
+  imports = [ ../../modules/home-manager ];
 
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
