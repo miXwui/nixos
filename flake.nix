ec58a9d94ae8e45747fe669a9e0f43771ac6ddfd
Michael Wu <me@miXwui.com>
2024-10-24 01:53:23 -0500

    Make flake inputs follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -5,10 +5,17 @@
   # For totally unstable packages [2]
 
   # Useful to show `follows`: `nix flake metadata`.
-  # However, we're not manually setting any `follows`.
+  # ~~However, we're not manually setting any `follows`.
   # This is to avoid the possible headache of build conflicts, trading for
-  # larger download sizes and disk space usage.
-  
+  # larger download sizes and disk space usage.~~
+  #
+  # Actually, this error with `sops-nix` still occurred without `follows`:
+  # ```
+  # error: builder for '/nix/store/a3gfziqmfsydr5mfv087y9q95r7kn0zd-manifest.json.drv' failed with exit code 127;
+  #     last 1 log lines:
+  #     > /build/.attr-0l2nkwhif96f51f4amnlf414lhl4rv9vh8iffyp431v6s28gsr90: line 14: /nix/store/2ay4jk44gqvwxikdhaspz4f9fhldxzgj-sops-install-secrets-0.0.1/bin/sops-install-secrets: No such file or directory
+  # ```
+
   inputs = {
     # [1]
     # Nix packages and unstable
@@ -19,37 +26,43 @@
     # Switched totally to unstable (because of issues like Mesa mismatch:
     # https://github.com/NixOS/nixpkgs/issues/343806).
     # Should be reliable. If an update breaks, wait until the issue's fixed.
+    #
+    # > Between nixpkgs-unstable and master is about 3 days and a binary cache
+    # > And then like 1-2 more days till nixos-unstable
+    # `nixos-unstable` also has critical NixOS tests to lessen breaking updates.
+    # https://www.reddit.com/r/NixOS/comments/1f46b04/comment/lkiww1t/
+    #
+    # Also see: https://status.nixos.org/
+
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
     # nixos-generators
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
-      # inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # Home Manager
     home-manager = {
       # url = "github:nix-community/home-manager/release-24.05"; # [1]
       url = "github:nix-community/home-manager"; # [2]
-      # inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # Nix hardware
-    nixos-hardware = {
-      url = "github:NixOS/nixos-hardware";
-      # inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
 
     # sops-nix
     sops-nix = {
       url = "github:Mic92/sops-nix/";
       # inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # https://github.com/nix-community/emacs-overlay/issues/396#issuecomment-2071348230
     emacs-overlay = {
       url = "github:nix-community/emacs-overlay";
-      # inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
