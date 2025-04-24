4031b46406b216e799a76c0b084d798c0e7cbaaf
Michael Wu <me@miXwui.com>
2025-04-24 07:27:14 -0500

    Revert `john_with-bsddb3` to `john`
    John the Ripper's `bitcoin2john.py` requires bsddb3 which is currently unsupported on Python 3.12.
    
    `berkeleydb` is supposed to be the replacement?
    
    Don't have the fix, so reverting to the standard package for now.

--- modules/home-manager/security.nix
+++ modules/home-manager/security.nix
@@ -1,17 +1,29 @@
-{ pkgs, ... }:
+{ pkgs, john, ... }:
 let
-  # Needs bsddb3 for `bitcoin2john.py`.
-  # See: https://github.com/openwall/john/issues/4143
+  # Needs bsddb3, but newer berkeleydb for `bitcoin2john.py` on Python 3.12?
+  # See:
+  # - https://github.com/openwall/john/issues/4143
+  # - https://github.com/NixOS/nixpkgs/pull/311198#discussion_r1599257522
+  # - https://github.com/openwall/john/commit/2778d2e9df4aa852d0bc4bfbb7b7f3dde2935b0c
+  # This code might need to be updated:
+  # - https://github.com/openwall/john/blob/bleeding-jumbo/run/bitcoin2john.py
   # TODO: PR to nixpkgs?
   john_with-bsddb3 = pkgs.john.overrideAttrs (previousAttrs: {
-    propagatedBuildInputs = previousAttrs.propagatedBuildInputs ++ [
-      (pkgs.python3.withPackages (ps: with ps; [ bsddb3 ]))
-    ];
+    # propagatedBuildInputs = previousAttrs.propagatedBuildInputs ++ [
+    #   (pkgs.python3.withPackages (ps: with ps; [ bsddb3 ]))
+    # ];
+    # `bsddb3` doesn't work on Python 3.12 due to
+    # https://github.com/NixOS/nixpkgs/issues/308232
+    # So may need to use berkeleydb?
+    # propagatedBuildInputs = previousAttrs.propagatedBuildInputs ++ [
+    #   (pkgs.python3.withPackages (ps: with ps; [ berkeleydb ]))
+    # ];
   });
 in
 {
   _module.args = {
-    john = john_with-bsddb3;
+    # john = john_with-bsddb3;
+    john = pkgs.john;
   };
 
   home.packages = with pkgs; [
